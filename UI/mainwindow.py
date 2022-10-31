7ce1ca069911edb1ad9023d58c8b3418a3afe448
Jeffrey <979284961@qq.com>
2022-10-31 19:31:24 +0800

    10.31 初步完成多线程

--- UI/mainwindow.py
+++ UI/mainwindow.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'mainGUI.ui'
+# Form implementation generated from reading ui file 'mainwindow.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.4
 #
